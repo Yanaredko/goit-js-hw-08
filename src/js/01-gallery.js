092aeed552df8d497b2a7aef164e1dbf27625b1d
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-08-16 18:29:45 +0300

    first task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const refs = {
+    gallery: document.querySelector('.gallery'),
+};
 
-console.log(galleryItems);
+function createGalleryMarkup(items) {
+    return items
+        .map(({ preview, original, description }) => `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" 
+                        src="${preview}" 
+                        alt="${description}" 
+                        data-source="${original}"/>
+                </a>
+            </li>
+        `)
+        .join("");
+}
+
+refs.gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
