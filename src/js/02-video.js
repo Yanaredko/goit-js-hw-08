de8994ffa700f1cf9e0a14d16aa703015ecc6437
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-08-16 18:34:13 +0300

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Vimeo from '@vimeo/player';
+
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+
+const player = new Vimeo(iframe);
+
+const saveTimeThrottled = throttle(saveTime, 1000);
+
+function saveTime(time) {
+  localStorage.setItem('videoplayer-current-time', time);
+}
+
+function handleTimeUpdate(event) {
+  const currentTime = event.seconds; 
+
+  saveTimeThrottled(currentTime);
+}
+
+function restorePlaybackTime() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+
+  if (savedTime !== null) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
+}
+
+player.ready().then(() => {
+    player.on('timeupdate', handleTimeUpdate);
+    
+      restorePlaybackTime();
+});
 
