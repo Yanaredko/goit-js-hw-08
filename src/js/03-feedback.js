8ab54de51241bd9427af0ad49a0d99c4748cb40a
Yanaredko <136071376+Yanaredko@users.noreply.github.com>
2023-08-16 18:37:20 +0300

    third task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash/throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+function saveFormState() {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function populateFormFromStorage() {
+  const storedData = localStorage.getItem('feedback-form-state');
+  if (storedData) {
+    const formData = JSON.parse(storedData);
+    emailInput.value = formData.email;
+    messageTextarea.value = formData.message;
+  }
+}
+
+function clearFormState() {
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageTextarea.value = '';
+}
+
+const throttledSaveFormState = throttle(saveFormState, 500);
+
+form.addEventListener('input', () => {
+  throttledSaveFormState();
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log('Form data:', formData);
+  clearFormState();
+});
+
+window.addEventListener('load', () => {
+  populateFormFromStorage();
+});
